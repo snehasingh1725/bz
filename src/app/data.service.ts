a03680d89d14fc4c0795d2100e7d15601161cfa9
snehasingh/1725 <savisneha16@gmail.com>
2020-01-08 12:39:52 +0530

    done

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
+'@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getCountries()
+  {
+    return this.http.get('https://restcountries.eu/rest/v2/all');
+  }
+  getUsers()
+  {
+
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
 }
